37eb6c6d5001453b52c34fd3ee785f9bcb9ae72f
Robiul Hasan <muhammadrobiul502@gmail.com>
2024-03-27 05:02:16 +0600

    added history block

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 
 
 const spinner = display => {
-    const spinner = document.getElementById('spinner');
-    spinner.style.display = display;
+  const spinner = document.getElementById('spinner');
+  spinner.style.display = display;
 }
 
 const allPlayers = () => {
-    document.getElementById('player-container').innerHTML = '';
-    const searchValue = document.getElementById('search-box').value;
-    document.getElementById('search-box').value = '';
-    spinner('block');
+  document.getElementById('player-container').innerHTML = '';
+  const searchValue = document.getElementById('search-box').value;
+  searchHistory(searchValue);
+  document.getElementById('search-box').value = '';
+  spinner('block');
 
-    const url = `https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=${searchValue}`;
-    
-    fetch(url)
+  const url = `https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=${searchValue}`;
+
+  fetch(url)
     .then(response => response.json())
     .then(data => displayPlayer(data.player))
 };
 
 const displayPlayer = players => {
-    const playerContainer = document.getElementById('player-container');
-    players?.forEach(player => {
-      console.log(player)
-        const div = document.createElement('div');
-        div.innerHTML = `
+  const playerContainer = document.getElementById('player-container');
+  players?.forEach(player => {
+    console.log(player)
+    const div = document.createElement('div');
+    div.innerHTML = `
         <div class="card border p-5 m-3">
         <div class="pro-pic">
           <img class="w-25" src="${player.strThumb}" alt="" />
@@ -36,24 +37,24 @@ const displayPlayer = players => {
         </div>
       </div>
         `;
-        spinner('none');
-        playerContainer.appendChild(div);
-    });
-    console.log(players);
+    spinner('none');
+    playerContainer.appendChild(div);
+  });
+  console.log(players);
 }
 
 const details = playerId => {
-    const url = `https://www.thesportsdb.com/api/v1/json/3/lookupplayer.php?id=${playerId}`;
-    fetch(url)
+  const url = `https://www.thesportsdb.com/api/v1/json/3/lookupplayer.php?id=${playerId}`;
+  fetch(url)
     .then(response => response.json())
     .then(data => showDetail(data.players[0]))
 }
 
 const showDetail = player => {
-   const detailContainer = document.getElementById('player-detail');
-   const div = document.createElement('div');
-   div.classList.add('w-100','mx-auto')
-   div.innerHTML = `
+  const detailContainer = document.getElementById('player-detail');
+  const div = document.createElement('div');
+  div.classList.add('w-100', 'mx-auto')
+  div.innerHTML = `
    <div class="card mx-auto " style="width: 18rem">
    <img src="${player.strThumb}" class="card-img-top" alt="..." />
    <div class="card-body">
@@ -68,5 +69,32 @@ const showDetail = player => {
    </div>
  </div>
    `;
-   detailContainer.appendChild(div);
+  detailContainer.appendChild(div);
+}
+
+const searchHistory = (value) => {
+  let history = [];
+  if (localStorage.getItem('searchHistory')) {
+    history = JSON.parse(localStorage.getItem('searchHistory'));
+  }
+  history.push(value);
+  localStorage.setItem('searchHistory', JSON.stringify(history));
+}
+
+// history block
+const historyContainer = document.querySelector("#history ul");
+const history = JSON.parse(localStorage.getItem('searchHistory'));
+
+if (history) {
+  document.getElementById('history-title').style.display = 'flex';
+  history.forEach(item => {
+    const li = document.createElement('li');
+    li.innerText = item;
+    historyContainer.appendChild(li);
+  });
+} 
+
+const clearHistory = () => {
+  localStorage.removeItem('searchHistory');
+  document.getElementById('history-title').style.display = 'none';
 }
